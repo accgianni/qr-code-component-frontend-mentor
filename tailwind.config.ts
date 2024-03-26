a1c8213660b4ce30657b4a8358eb89282f8b4f85
Gianni Accurso <gianniaccurso@MacBook-Air-de-Gianni.local>
2024-03-26 17:22:56 +0100

    initial-commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,12 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        white: "hsl(0, 0%, 100%)",
+        lightGray: "hsl(212, 45%, 89%)",
+        grayishBlue: "hsl(220, 15%, 55%)",
+        darkBlue: "hsl(218, 44%, 22%)",
+      },
     },
   },
   plugins: [],
